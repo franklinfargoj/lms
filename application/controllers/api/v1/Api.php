290bf0e739e4a6adbc915700fe6ecaa10f58f4d9
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-09-10 14:43:51 +0530

    change for elapsed day count

--- application/controllers/api/v1/Api.php
+++ application/controllers/api/v1/Api.php
@@ -781,7 +781,7 @@ class Api extends REST_Controller
     }
 
     /**
-     * unassigned_leads
+                 * unassigned_leads
      * loads the unassigned leads count filtered by lead source
      * @autor Gourav Thatoi
      * @accss public
@@ -795,7 +795,7 @@ class Api extends REST_Controller
             $table = Tbl_Leads;
             $join = array('db_lead_assign', 'db_lead_assign.lead_id = db_leads.id ', 'left');
             $group_by = array('db_leads.lead_source');
-            $where = array(Tbl_Leads . '.branch_id' => $params['branch_id'], Tbl_LeadAssign . '.lead_id' => NULL, 'YEAR(' . Tbl_Leads . '.created_on)' => date('Y'));
+            $where = array(Tbl_Leads . '.branch_id' => $params['branch_id'], Tbl_LeadAssign . '.lead_id' => NULL, 'YEAR(' . Tbl_Leads . '.created_on)' => date('Y'),'DATEDIFF( CURDATE( ) , '.Tbl_Leads.'.created_on) <=' => Elapsed_day);
             $arrData['unassigned_leads_count'] = $this->Lead->unassigned_status_count($select, $table, $join, $where, $group_by);
             $response = array();
             $keys = array('Walk-in' => "0", 'Analytics' => "0", 'Tie Ups' => "0", 'Enquiry' => "0");
