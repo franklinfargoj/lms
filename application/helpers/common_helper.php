cf3c6cb885fe7fe88f35a3e4e916314ba55be6ae
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-09-11 12:36:23 +0530

    sms enable

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -444,17 +444,17 @@ if(!function_exists('send_sms')){
             $credentials = $CI->sms->get_sms_credentials();
             $password = $CI->encrypt->decode($credentials['password']);
             $url = $credentials['url'].'?username='.$credentials['username'].'&password='.$password.'&to='.$mobile.'&udh=&from=DENABK&text='.urlencode($message);
-//            $ch = curl_init();
-//            curl_setopt($ch, CURLOPT_URL, $url);
-//            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//            $output = curl_exec($ch);
-//            curl_close($ch);
-//            echo $output;
-//            die;
-            $output = file_get_contents($url);
-            echo $output;die;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $output = curl_exec($ch);
+            curl_close($ch);
+            echo $output;
+            die;
+//            $output = file_get_contents($url);
+//            echo $output;die;
 
         }
     }
