83b4bce2e9f8567beea5e890d18bca8899b3facd
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-09-11 12:11:28 +0530

    sms enable

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -445,13 +445,10 @@ if(!function_exists('send_sms')){
             $password = $CI->encrypt->decode($credentials['password']);
             $url = $credentials['url'].'?username='.$credentials['username'].'&password='.$password.'&to='.$mobile.'&udh=&from=DENABK&text='.$message;
             $ch = curl_init();
-            echo $ch;die;
             curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             $output = curl_exec($ch);
             curl_close($ch);
             return $output;
