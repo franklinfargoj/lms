fb882b73d3341485c89ef59051fe473c7fdd08b0
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-09-16 15:21:11 +0530

    notification

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -45,4 +45,41 @@ class Test extends CI_Controller
         // close socket
         socket_close($socket);
     }
+
+    function sendPushNotification()
+    {
+        //$d_type = ($device_type==0)? "appNameAndroid" : "appNameIOS";
+        // $collection = PushNotification::app($d_type)->to($device_id)->send($message);
+        // return $response = $collection->pushManager->getAdapter()->getResponse();
+
+        $url = 'https://fcm.googleapis.com/fcm/send';
+//    $server_key = 'AAAAJTxIDRs:APA91bGmPFIAFGn7ZMj1XX__Vw-ONFXBbUwsJp_F3qCBalPyYMhCWcRiNtj7l7PzuGKuwSyG950X8s1kYFMHQIVcyXhH-ylwcYBZzaPnpTGxKfB1yOeAVTEkyp69_jNc25QNroxb_b-Z';
+        $server_key = FCMKEY;
+        //$to = $device_id;
+        $to = 'dbEu5-lTFtQ:APA91bEfBDObFHTcwSrFcwqYYQZkUY2_ZBY4iYsmfp9QnMtNbO5xAndriCz5zB3P1fAqeYAc7-4a09aHhC8n1x569lSYDDcswHv_2vzvkVDaNDFZnISqzttIRyWnTdZbWmDg82FQnfqp';
+        $notification_title = 'Test Title';
+        $data = array('body'=>'hi mukesh', 'title' => $notification_title, "icon" => "myicon","notification_type"=>"action");
+
+        $fields = json_encode(array('to' => $to, 'data' => $data));
+        $headers = array(
+            'Content-Type:application/json',
+            'Authorization:key='.$server_key
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        $result = curl_exec($ch);
+
+        if ($result === FALSE) {
+            // die('FCM Send Error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+        return $result;
+    }
 }
