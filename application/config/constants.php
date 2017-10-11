45ddf28fa7ba477fce5893dad55b5ea08b4edd2d
gourav thatoi <gourav.thatoi@wwindia.com>
2017-10-11 12:58:18 +0530

    constant changes

--- application/config/constants.php
+++ application/config/constants.php
@@ -125,18 +125,13 @@ define('Tbl_emp_dump', 'employee_dump');
 //define('HRMS_API_URL_GET_RECORD', 'https://172.25.3.26:8445/PSIGW/RESTListeningConnector/DBK_LMS_EMP_RECORD1.v1/?emplid=');
 //define('HRMS_API_URL_AUTH', 'http://staging.php-dev.in:8844/DENABANK/lms/api/v1/authenticate/check_authentication');
 //define('HRMS_API_URL_GET_RECORD', 'http://staging.php-dev.in:8844/DENABANK/lms/api/v1/authenticate/get_records');
-//define('HRMS_API_URL_AUTH', 'http://103.224.110.52/lms/hrms_auth.php?');
-//define('HRMS_API_URL_GET_RECORD', 'http://103.224.110.52/lms/hrms_records.php?');
-//define('HRMS_BRANCH_RECORD', 'http://103.224.110.52/master.php');
-//define('FINACLE_ACCOUNT_RECORD', 'http://103.224.110.52/client.php?account_no=');
+define('HRMS_API_URL_AUTH', 'http://103.224.110.52/lms/hrms_auth.php?');
+define('HRMS_API_URL_GET_RECORD', 'http://103.224.110.52/lms/hrms_records.php?');
+define('HRMS_BRANCH_RECORD', 'http://103.224.110.52/master.php');
 define('Elapsed_day_converted', 7); //Elapsed Days for converted
 define('Elapsed_day_NI', 7); //Elapsed Days for Not Interested
 define('Elapsed_day', 90); //Elapsed Days for remaining all
-
-define('HRMS_API_URL_AUTH', 'http://103.224.110.52/lms/hrms_off_auth.php?');
-define('HRMS_API_URL_GET_RECORD', 'http://103.224.110.52/lms/hrms_off_records.php?');
-define('HRMS_BRANCH_RECORD', 'http://103.224.110.52/master.php');
-define('FINACLE_ACCOUNT_RECORD', 'http://103.224.110.52/client_off.php?account_no=');
+define('FINACLE_ACCOUNT_RECORD', 'http://103.224.110.52/client.php?account_no=');
 
 define('FCMKEY','AAAA-QhpGTY:APA91bE-AL5cp0mPgmxhm4M1pTPqzNVTl1a0PxS3ZSBmO4eA5crSstcDRsXOUR1JYp5mQsBUN7kgtPxCrsN0rx7BZ8aHDJzW5iJIcP6GU2hvCs_mu13rRfFHijeEoSwulG3A6OzrhNgP
 ');
