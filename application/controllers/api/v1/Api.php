f1dcce60ccda0d1747401446a0772ff4d7cc2b44
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-10-26 21:55:26 +0530

    comment point distribution function

--- application/controllers/api/v1/Api.php
+++ application/controllers/api/v1/Api.php
@@ -2485,7 +2485,7 @@ class Api extends REST_Controller
                      * Point distribution When status is converted
                      *****************************************************************/
                     if($params['status'] == 'Converted'){
-                        $this->points_distrubution($params['lead_id']);
+                        //$this->points_distrubution($params['lead_id']);
                     }
 
                     $cat_name = $params['category_title'];
