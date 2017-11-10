076053fc985cfd357562543d7e26795ffb818bcc
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-11-10 22:13:00 +0530

    Change as per new logic for pendency

--- application/controllers/Reports.php
+++ application/controllers/Reports.php
@@ -420,23 +420,25 @@ class Reports extends CI_Controller
         $arrData['Total'] = 0;
         if($list){
             $Lead['userId'] = array();
-            foreach ($leads as $key => $value) {
-                //Employee Login
-                if($viewName == 'EM'){
-                    $index = $value['employee_id'];
-                    $Lead['userId'][] = $value['employee_id'];
+            if(!empty($leads)) {
+                foreach ($leads as $key => $value) {
+                    //Employee Login
+                    if ($viewName == 'EM') {
+                        $index = $value['employee_id'];
+                        $Lead['userId'][] = $value['employee_id'];
+                    }
+                    //Branch Manager Login
+                    if ($viewName == 'BM') {
+                        $index = $value['branch_id'];
+                        $Lead['userId'][] = $value['branch_id'];
+                    }
+                    //Zone Manager Login
+                    if ($viewName == 'ZM') {
+                        $index = $value['zone_id'];
+                        $Lead['userId'][] = $value['zone_id'];
+                    }
+                    $Lead[$index]['lead_identification'][$value['lead_identification']] = $value['count'];
                 }
-                //Branch Manager Login
-                if($viewName == 'BM'){
-                    $index = $value['branch_id'];
-                    $Lead['userId'][] = $value['branch_id'];
-                }
-                //Zone Manager Login
-                if($viewName == 'ZM'){
-                    $index = $value['zone_id'];
-                    $Lead['userId'][] = $value['zone_id'];
-                }
-                $Lead[$index]['lead_identification'][$value['lead_identification']] = $value['count'];
             }
             $arrData['viewName'] = $viewName;
             foreach ($list as $key => $value) {
