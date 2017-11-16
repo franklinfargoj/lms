7bc066676761e7d16001e401bff97a4d1264e5af
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2017-11-16 21:05:42 +0530

    add No.of pending Leads before Documentation

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -113,26 +113,26 @@ class Cron extends CI_Controller
             //FOR ZONAL MANAGER
             $zonal_manager = array('generated' => array(),'converted' => array(),'unassigned' => array(),'pending_before' => array(),'pending' => array());
             $gm = $zonal_manager;
-            $branch_list = $this->Lead->get_employee_dump(array('DISTINCT(branch_id) as branch_id','branch_name'),array('zone_id' => '009846'),array(),'employee_dump');
+            $branch_list = $this->Lead->get_employee_dump(array('DISTINCT(branch_id) as branch_id','branch_name'),array('zone_id' => $v->zone_id),array(),'employee_dump');
 //            echo "<pre>";
 ////            echo $v->zone_id;
 //           print_r($branch_list);die;
-            $zonal_manager['generated']  = $this->get_leads(array('type'=>'generated','till'=>'mtd','user_type'=>'BM','zone_id' => '009846'));
-            $zonal_manager['converted']  = $this->get_leads(array('type'=>'converted','till'=>'mtd','user_type'=>'BM','zone_id' => '009846'));
-            $zonal_manager['unassigned'] = $this->get_leads(array('type'=>'unassigned','till'=>'','user_type'=>'BM','zone_id' => '009846'));
-            $zonal_manager['pending_before']   = $this->get_leads(array('type'=>'pending_before','till'=>'','user_type'=>'BM','zone_id' => '009846'));
-            $zonal_manager['pending']    = $this->get_leads(array('type'=>'pending','till'=>'TAT','user_type'=>'BM','zone_id' => '009846'));
+            $zonal_manager['generated']  = $this->get_leads(array('type'=>'generated','till'=>'mtd','user_type'=>'BM','zone_id' => $v->zone_id));
+            $zonal_manager['converted']  = $this->get_leads(array('type'=>'converted','till'=>'mtd','user_type'=>'BM','zone_id' => $v->zone_id));
+            $zonal_manager['unassigned'] = $this->get_leads(array('type'=>'unassigned','till'=>'','user_type'=>'BM','zone_id' => $v->zone_id));
+            $zonal_manager['pending_before']   = $this->get_leads(array('type'=>'pending_before','till'=>'','user_type'=>'BM','zone_id' => $v->zone_id));
+            $zonal_manager['pending']    = $this->get_leads(array('type'=>'pending','till'=>'TAT','user_type'=>'BM','zone_id' => $v->zone_id));
 
             //$zonal_manager = call_user_func_array('array_merge', $zonal_manager);
             $zonal_manager = array_merge($zonal_manager);
-            pe($zonal_manager);
+            //pe($zonal_manager);
             $total = array();
             foreach (array_keys($gm) as $key => $value) {
                 if(!empty($zonal_manager[$value])){
                     $total[$value] = array_column($zonal_manager[$value], $value,'branch_id');
                 }
             }
-           pe($total);die;
+//            pe($total);die;
             $unique_branch_ids = array_unique(array_column($zonal_manager, 'branch_id'));
             foreach ($branch_list as $key => $value) {
 
@@ -487,7 +487,7 @@ class Cron extends CI_Controller
                 $where['l.created_by_zone_id !='] = NULL;
                 $group_by = array('l.created_by_zone_id');
             }elseif($user_type == 'BM'){
-                $select[] = 'l.created_by_branch_id  as branch _id';
+                $select[] = 'l.created_by_branch_id  as branch_id';
                 $where['l.created_by_zone_id'] = $zone_id;
                 $where['l.created_by_branch_id !='] = NULL;
                 $group_by = array('l.created_by_branch_id');
