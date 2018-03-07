c0d63ead0d94dd6f2ac5bc436aca55aeb832654f
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2018-03-07 10:57:25 +0530

    Api for landperfect

--- application/controllers/api/v1/Other_api.php
+++ application/controllers/api/v1/Other_api.php
@@ -60,22 +60,25 @@ class Other_api extends REST_Controller
         $params = $this->input->post();
         if (!empty($params) && isset($params['date']) && !empty($params['date'])) {
             $date = $params['date'];
-            $now = time();
-            $your_date = strtotime($date);
-            $datediff = $now - $your_date;
-            $days =  round($datediff / (60 * 60 * 24));
-            if($days >30){
-                $res = array('result' => False,
-                    'data' => array('Date not more than 30 days old from today'));
-                returnJson($res);
-            }
+            $previous_date = date('Y-m-d', strtotime('-10 days',strtotime($date)));
+//            echo $previous_date;die;
+//            $now = time();
+//            $your_date = strtotime($date);
+//            $datediff = $now - $your_date;
+//            $days =  round($datediff / (60 * 60 * 24));
+//            if($days >30){
+//                $res = array('result' => False,
+//                    'data' => array('Date not more than 30 days old from today'));
+//                returnJson($res);
+//            }
             $action = 'list';
             $select = array('lead.id','lead.customer_name','lead.contact_no','lead.created_by_branch_id','lead.branch_id as current_branch_id','lead.created_by','lead.reroute_from_branch_id','lead.created_on','products.title as product','product_category.title as category','lead.id');
             $table = Tbl_Leads.' AS lead';
             $join[] = array('table' =>Tbl_Products.' AS products','on_condition'=>'products.id = lead.product_id','type'=>'left');
             $join[] = array('table' =>Tbl_Category.' AS product_category','on_condition'=>'product_category.id = lead.product_category_id','type'=>'left');
-            $where = array('lead.reroute_from_branch_id !='=>NULL);
-            $where["date(lead.created_on) >='".$date."' AND date(lead.created_on) <='".date('Y-m-d')."'"] = NULL;
+            $join[] = array('table' => Tbl_LeadAssign.' as la','on_condition' => 'la.lead_id = lead.id','type' => 'LEFT');
+            $where = array('la.status NOT IN ("NC","DC","AO","Converted","Closed","NI")' => NULL);
+            $where["date(lead.created_on) <='".$date."' AND date(lead.created_on) >='".$previous_date."' AND products.id IN(23,24,25,26,29,30)"] = NULL;
             $order_by = 'lead.created_on DESC';
             $generated_leads = $this->Lead->get_leads($action,$table,$select,$where,$join,$group_by=array(),$order_by);
             if(!empty($generated_leads)) {
