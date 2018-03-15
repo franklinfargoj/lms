02e6f08d041fec661bae5faffbd86844c3f08f3b
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2018-03-15 12:53:37 +0530

    change

--- hrms_records.php
+++ hrms_records.php
@@ -1,5 +1,6 @@
 <?php
 $url1 = 'https://103.224.111.35/lms/hrms_records.php?hrms_id='.$_GET['hrms_id'];
+echo $url1;
 $ch1 = curl_init();
 curl_setopt($ch1,CURLOPT_URL,$url1);
 curl_setopt($ch1,CURLOPT_POST,0);
