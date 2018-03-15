2755afc20ef6f549c2b0b6a4e5a6037ad9cc502f
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2018-03-15 12:14:37 +0530

    extra file to fetch hrms details

---
+++ lms/hrms_auth.php
@@ -0,0 +1,10 @@
+<?php
+$url = 'https://103.224.111.35/lms/hrms_auth.php?username='.$_GET['username'].'&password='.$_GET['password'];
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_POST,0);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_exec($ch);
+$result = curl_exec($ch);
+echo $result;
+?>
