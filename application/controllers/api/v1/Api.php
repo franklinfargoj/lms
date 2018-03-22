91a0af3929927ea9d4b801e386104fc54d0b5173
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2018-03-22 19:16:14 +0530

    count

--- application/controllers/api/v1/Api.php
+++ application/controllers/api/v1/Api.php
@@ -891,7 +891,7 @@ class Api extends REST_Controller
             $table = Tbl_Leads;
             $join = array('db_lead_assign', 'db_lead_assign.lead_id = db_leads.id ', 'left');
             $group_by = array('db_leads.lead_source');
-            $where = array(Tbl_Leads . '.branch_id' => $params['branch_id'], 'YEAR(' . Tbl_Leads . '.created_on)' => date('Y'), 'DATEDIFF( CURDATE( ) , ' . Tbl_Leads . '.created_on) <=' => Elapsed_day);
+            $where = array(Tbl_Leads . '.branch_id' => $params['branch_id']);
             $where['('.Tbl_LeadAssign.'.lead_id IS NULL OR '.Tbl_LeadAssign.'.is_deleted = 1)'] = NULL;
             
 $arrData['unassigned_leads_count'] = $this->Lead->unassigned_status_count($select, $table, $join, $where, $group_by);
