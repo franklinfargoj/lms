1fb1fd1a03c8e0359fdcc10f0ed703bd479345d8
Mukesh Kurmi <mukesh.kurmi@wwindia.com>
2018-03-30 11:56:24 +0530

    Add function followup scheduler

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -17,6 +17,7 @@ class Cron extends CI_Controller
         parent::__construct();
        is_cli() OR show_404();
         $this->load->model('Lead');
+        $this->load->model('Reminder_model');
     }
 
     /*
@@ -1270,6 +1271,28 @@ $pending_days = 2;
                 This is an auto generated e-mail escalated to you on account of pendency beyond defined TAT at Branch/DZM/ZM level.<br><br>
                 Regards,<br>Dena Sampark";
         return $msg;
+    }
+
+    /*
+     * current_day_schedules
+     * Current day employee schedule
+     * @author Franklin Fargoj
+     * @access private
+     * @param none
+     * @return void
+     */
+    public function current_day_schedules(){
+        $today_schedule = $this->Reminder_model->get_current_schedule();
+
+        //pe($today_schedule);die;
+        //$today_schedule[0]['remind_to']; //pe($today_schedule);die;
+        if (count($today_schedule) > 0) {
+            foreach ($today_schedule as $key => $value) {
+                $contact_no= $this->Lead->get_employee_dump(array('contact_no'),array('hrms_id' => $value['remind_to']),array(),'employee_dump');
+                $contact=$contact_no[0]->contact_no;
+                send_sms($contact,$value['reminder_text']);
+            }
+        }
     }
 
 }
