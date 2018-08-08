1fdd08edebb3b5216d4e2583cdb5f34cb3a44bc7
Franklin <franklin.fargoj@neosofttech.com>
2018-08-08 13:17:38 +0530

    Changed tie_ups to Tie Ups and changed in db also

--- application/views/middle/Leads/upload.php
+++ application/views/middle/Leads/upload.php
@@ -11,14 +11,15 @@ $lead_source[''] = 'Select Lead Source';
 $lead_source['Tie Ups'] = 'Tie Ups';
 $lead_source['Enquiry'] = 'Enquiry';
 $lead_source['Analytics'] = 'Analytics';
-$source_options[''] = 'Select Lead Source';
+
 //$source_options['Tie Ups'] = 'Tie Ups';
 //$source_options['Enquiry'] = 'Enquiry';
 //$source_options['Analytics'] = 'Analytics';
+$source_options[''] = 'Select Lead Source';
 $source_options['enquiry'] = 'Website,IVR';
 $source_options['analytics'] = 'Analytics';
+$source_options['Tie Ups'] = 'Others';
 //$source_options['walkin'] = 'Branch Generated';
-$source_options['tie_ups'] = 'Others';
 $data_submit = array(
     'name' => 'Submit',
     'id' => 'Submit',
