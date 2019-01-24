379dfef889671e91fa4a99e707762c896ecdaa50
Franklin <franklin.fargoj@neosofttech.com>
2019-01-24 15:45:18 +0530

    testing session

--- application/views/middle/Leads/unassigned_list.php
+++ application/views/middle/Leads/unassigned_list.php
@@ -150,7 +150,7 @@ $source = $this->config->item('lead_source');
                     ?>
                             <tr>
                                 <td  style="text-align:center">
-                                    <?php if($_SESSION['is_rapc']=='Y' || $value['lead_source'] != 'walkin' || ($value['lead_source'] == 'walkin' && $value['mapping'] == 'BRANCH' || ($value['reroute_from_branch_id'] != '' || $value['reroute_from_branch_id'] != NULL))) {
+                                    <?php pe($_SESSION['is_rapc']); if($_SESSION['is_rapc']=='Y' || $value['lead_source'] != 'walkin' || ($value['lead_source'] == 'walkin' && $value['mapping'] == 'BRANCH' || ($value['reroute_from_branch_id'] != '' || $value['reroute_from_branch_id'] != NULL))) {
 
                                     $data = array(
                                         'name' => 'lead_ids[]',
